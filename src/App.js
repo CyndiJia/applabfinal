5a4f52376af5d12bff2bd0767ee12a6a73fddc55
Cyndi <31037969+CyndiJia@users.noreply.github.com>
2019-11-27 09:56:25 +0800

    speech&video basic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import Speech from 'react-speech';
+import { Player } from 'video-react';
+//import logo from './logo.svg';
+// import '~video-react/dist/video-react.css'; 
 import './App.css';
+
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Speech
+          text="Pat pat, Yiqi"
+          pitch="0.8"
+          rate="1"
+          volume="1"
+          lang="en-GB"
+          voice="Google UK English Female"
+        />
+      <Player
+          playsInline
+          poster="/assets/poster.png"
+          src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+    />
     </div>
   );
 }
